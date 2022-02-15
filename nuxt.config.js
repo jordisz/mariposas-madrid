c2fa49d666fc1893862731d56b616cd684d2908a
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2022-02-15 17:17:00 +0100

    Improve configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/mariposas-madrid/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
